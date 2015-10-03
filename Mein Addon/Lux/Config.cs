e49eb1ee3809e71e703f4e5489aa52410000bea2
KasaiPlays <ZhierdLoL@hotmail.com>
2015-10-03 16:14:01 +0200

    Erfolgreich Hochgeladen!

---
+++ Mein Addon/Lux/Config.cs
@@ -0,0 +1,131 @@
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace AddonTemplate
+{
+
+    public static class Config
+    {
+        private const string MenuName = "HardcoreLux";
+
+        private static readonly Menu Menu;
+
+        static Config()
+        {
+            // Initialize the menu
+            Menu = MainMenu.AddMenu(MenuName, MenuName.ToLower());
+            Menu.AddGroupLabel("Welcome to this AddonTemplate!");
+            Menu.AddLabel("To change the menu, please have a look at the");
+            Menu.AddLabel("Config.cs class inside the project, now have fun!");
+
+            // Initialize the modes
+            Modes.Initialize();
+        }
+
+        public static void Initialize()
+        {
+        }
+
+        public static class Modes
+        {
+            private static readonly Menu Menu;
+
+            static Modes()
+            {
+                
+                Menu = Config.Menu.AddSubMenu("Modes");
+
+                
+                Combo.Initialize();
+                Menu.AddSeparator();
+
+                
+                Harass.Initialize();
+            }
+
+            public static void Initialize()
+            {
+            }
+
+            public static class Combo
+            {
+                private static readonly CheckBox _useQ;
+                private static readonly CheckBox _useW;
+                private static readonly CheckBox _useE;
+                private static readonly CheckBox _useR;
+
+                public static bool UseQ
+                {
+                    get { return _useQ.CurrentValue; }
+                }
+                public static bool UseW
+                {
+                    get { return _useW.CurrentValue; }
+                }
+                public static bool UseE
+                {
+                    get { return _useE.CurrentValue; }
+                }
+                public static bool UseR
+                {
+                    get { return _useR.CurrentValue; }
+                }
+
+                static Combo()
+                {
+                    
+                    Menu.AddGroupLabel("Combo");
+                    _useQ = Menu.Add("comboUseQ", new CheckBox("Use Q"));
+                    _useW = Menu.Add("comboUseW", new CheckBox("Use W"));
+                    _useE = Menu.Add("comboUseE", new CheckBox("Use E"));
+                    _useR = Menu.Add("comboUseR", new CheckBox("Use R", false)); 
+                }
+
+                public static void Initialize()
+                {
+                }
+            }
+
+            public static class Harass
+            {
+                public static bool UseQ
+                {
+                    get { return Menu["harassUseQ"].Cast<CheckBox>().CurrentValue; }
+                }
+                public static bool UseW
+                {
+                    get { return Menu["harassUseW"].Cast<CheckBox>().CurrentValue; }
+                }
+                public static bool UseE
+                {
+                    get { return Menu["harassUseE"].Cast<CheckBox>().CurrentValue; }
+                }
+                public static bool UseR
+                {
+                    get { return Menu["harassUseR"].Cast<CheckBox>().CurrentValue; }
+                }
+                public static int Mana
+                {
+                    get { return Menu["harassMana"].Cast<Slider>().CurrentValue; }
+                }
+
+                static Harass()
+                {
+                    
+                    Menu.AddGroupLabel("Harass");
+                    Menu.Add("harassUseQ", new CheckBox("Use Q"));
+                    Menu.Add("harassUseW", new CheckBox("Use W"));
+                    Menu.Add("harassUseE", new CheckBox("Use E"));
+                    Menu.Add("harassUseR", new CheckBox("Use R", false)); 
+
+                    
+                    Menu.Add("harassMana", new Slider("Maximum mana usage in percent ({0}%)", 40));
+                }
+
+                public static void Initialize()
+                {
+                }
+            }
+        }
+    }
+}
